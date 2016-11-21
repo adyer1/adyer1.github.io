cc673d520c730f6f4567eb4ff6b067d49935b473
Alison Dyer <adyer@pratt.edu>
2016-11-21 17:02:33 +0000

    index.html

--- pantree/js/main.js
+++ pantree/js/main.js
@@ -1,1 +1,27 @@
-alert("Hello World!");
+$(document).ready(function() {
+    
+    $('form').submit(function(event) {
+
+        var formData = $(this).serialize();
+      
+        $.ajax({
+            type         : 'POST',
+            url          : 'https://web2-product-page/herokuapp.com/subscribers', 
+            data         : formData, 
+            dataType     : 'json' 
+        }).done(function(data) {
+
+                console.log(data);
+                $(".confirmation").fadeIn();
+                $(".error-message").text("");
+                $("input[name=email]").val("");
+            }).fail(function(data) {
+                console.log(data);
+                var errorMessage = JSON.parse(data.responseText).email[0];
+                $(".error-message").text(errorMessage);
+                $(".confirmation").hide();
+            });
+    
+        event.preventDefault();
+    });    
+});
