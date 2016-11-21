affacdd7a7868faaeef6c86af26c0b72bd7764b4
Alison Dyer <adyer@pratt.edu>
2016-11-21 18:11:25 +0000

    index.html

--- pantree/js/main.js
+++ pantree/js/main.js
@@ -5,22 +5,24 @@ $(document).ready(function() {
         var formData = $(this).serialize();
       
         $.ajax({
-            type         : 'POST',
-            url          : 'https://web2-product-page/herokuapp.com/subscribers', 
-            data         : formData, 
-            dataType     : 'json' 
-        }).done(function(data) {
+        type         : 'POST',
+        url          : 'https://web2-product-page.herokuapp.com/subscribers', 
+        data         : formData, 
+        dataType     : 'json' 
+    }).done(function(data) {
 
-                console.log(data);
-                $(".confirmation").fadeIn();
-                $(".error-message").text("");
-                $("input[name=email]").val("");
-            }).fail(function(data) {
-                console.log(data);
-                var errorMessage = JSON.parse(data.responseText).email[0];
-                $(".error-message").text(errorMessage);
-                $(".confirmation").hide();
-            });
+            if (data.validEmail) {
+                $('.confirmation').fadeIn();
+                $('.error-message').text("");
+                $('input[name=email]').val("");
+            } else {
+                $('.error-message').text("Not a valid email address");
+            }
+
+
+        }).fail(function() {
+            $('.error-message').text("The server did not respond.");                
+        });
     
         event.preventDefault();
     });    
